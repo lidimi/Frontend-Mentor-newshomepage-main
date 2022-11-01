68edc5d7be8dd05e7125c1d7b23c63fe39a10304
lidimi <lidsheq97@gmail.com>
2022-11-01 20:34:55 +0100

    styles update

--- javas.js
+++ javas.js
@@ -1,6 +1,7 @@
 const openBtn = document.querySelector('.open-menu');
 const closeBtn = document.querySelector('.close-btn');
 const mobileMenu = document.querySelector('#mobile-menu');
+const pageShadow = document.querySelector('#page-shadow');
 
 
 openBtn.addEventListener('click', () => {
